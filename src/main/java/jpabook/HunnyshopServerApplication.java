cb3a9036a051a3ad92d6da80efc035dfe102339b
yeonghoon.lee <djaakclsrn12@naver.com>
2022-12-16 19:01:02 +0900

    NEW: initial commit

---
+++ src/main/java/jpabook/HunnyshopServerApplication.java
@@ -0,0 +1,13 @@
+package jpabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HunnyshopServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HunnyshopServerApplication.class, args);
+    }
+
+}
