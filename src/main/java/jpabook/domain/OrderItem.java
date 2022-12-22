9da637943c400cd0dc9b0e8cb8816a12bd0572cc
yeonghoon.lee <djaakclsrn12@naver.com>
2022-12-23 00:05:11 +0900

    NEW: 주문 아이템 매핑 entity 작성

---
+++ src/main/java/jpabook/domain/OrderItem.java
@@ -0,0 +1,30 @@
+package jpabook.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import jpabook.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+    @ManyToOne
+    @JoinColumn(name ="order_id")
+    private Order oder;
+    private int orderPrice;
+    // 주문수량
+    private int count;
+}
