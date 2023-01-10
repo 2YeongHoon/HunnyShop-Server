f6472286e2e537a78e34f9244702789aa15e9704
sanynote <sunnyjuice94@gmail.com>
2023-01-10 23:47:20 +0900

    NEW: 주문 레포지토리

---
+++ src/main/java/jpabook/repository/OrderRepository.java
@@ -0,0 +1,21 @@
+package jpabook.repository;
+
+import javax.persistence.EntityManager;
+import jpabook.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+  private final EntityManager entityManager;
+
+  public void save(Order order){
+    entityManager.persist(order);
+  }
+
+  public Order findOne(Long id){
+    return entityManager.find(Order.class, 1);
+  }
+}
