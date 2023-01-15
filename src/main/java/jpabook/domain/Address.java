fa0ebee87bd04a7a29baa2e42f60c4d4a3a90827
yeonghoon.lee <djaakclsrn12@naver.com>
2023-01-15 21:41:05 +0900

    BUG: 생성자 추가

--- src/main/java/jpabook/domain/Address.java
+++ src/main/java/jpabook/domain/Address.java
@@ -10,4 +10,14 @@ public class Address {
     private String city;
     private String street;
     private String zipCode;
+
+    public Address() {
+    }
+
+    public Address(String city, String street, String zipCode) {
+        this.city = city;
+        this.street = street;
+        this.zipCode = zipCode;
+    }
+
 }
