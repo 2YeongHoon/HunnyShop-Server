a5409c4b954f18decc939f25f40fab7bcb27812b
yeonghoon.lee <djaakclsrn12@naver.com>
2023-01-23 22:03:25 +0900

    NEW: 상품 컨트롤러 추가

---
+++ src/main/java/jpabook/controller/ItemController.java
@@ -0,0 +1,38 @@
+package jpabook.controller;
+
+import java.util.List;
+import jpabook.domain.item.Book;
+import jpabook.domain.item.Item;
+import jpabook.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.save(book);
+        return "redirect:/";
+    }
+}
