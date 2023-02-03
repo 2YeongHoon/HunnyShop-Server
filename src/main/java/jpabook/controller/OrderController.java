1b7e46d41a73aa8f574fff141d59db09e486ed85
yeonghoon.lee <djaakclsrn12@naver.com>
2023-02-03 23:08:27 +0900

    NEW: 주문 리스트 조회 기능 추가

--- src/main/java/jpabook/controller/OrderController.java
+++ src/main/java/jpabook/controller/OrderController.java
@@ -2,7 +2,9 @@ package jpabook.controller;
 
 import java.util.List;
 import jpabook.domain.Member;
+import jpabook.domain.Order;
 import jpabook.domain.item.Item;
+import jpabook.repository.OrderSearch;
 import jpabook.service.ItemService;
 import jpabook.service.MemberService;
 import jpabook.service.OrderService;
@@ -10,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -42,4 +46,17 @@ public class OrderController {
     return "redirect:/orders";
   }
 
+  @GetMapping("/orders")
+  public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+    List<Order> orders = orderService.findOrders(orderSearch);
+    model.addAttribute("orders", orders);
+
+    return "order/orderList";
+  }
+
+  @PostMapping("/orders/{orderId}/cancel")
+  public String cancelOrder(@PathVariable("orderId") Long orderId) {
+    orderService.cancelOrder(orderId);
+    return "redirect:/orders";
+  }
 }
